0f2f4f55264cfb0e6ac063181cfc16630116580c
javier <javier@icimaf.cu>
2023-07-14 22:59:51 -0400

    sandbox created

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+/** @type {import('jest').Config} */
+const config = {
+  verbose: true,
+  // ...otras configuraciones
 };
+
+module.exports = config;
